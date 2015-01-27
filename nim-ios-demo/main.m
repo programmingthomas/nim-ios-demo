e2adecc2a7fb3ebe18e736ae55c1ff6c25de83e8
Programming Thomas <programmingthomas@gmail.com>
2015-01-27 16:37:34 +0000

    Call to NimMain for GC

--- nim-ios-demo/main.m
+++ nim-ios-demo/main.m
@@ -8,9 +8,11 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+#import "backend.h"
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        NimMain();
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
